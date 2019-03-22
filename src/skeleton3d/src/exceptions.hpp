7079594b9ab05d914ca8f17586bd2c23e0d0ce47
Lukas Kalde <lkalde@gmail.com>
2019-03-22 11:56:53 +0100

    Documentation for exception, moved no-longer-needed class to other package

--- src/skeleton3d/src/exceptions.hpp
+++ src/skeleton3d/src/exceptions.hpp
@@ -1,16 +1,22 @@
 #ifndef SKELETON_EXCEPTIONS
 #define SKELETON_EXCEPTIONS
 
+/**
+ * Define all new exceptions needed for this package.
+ */
 namespace skeleton_exceptions
 {
+    /**
+     * This exception should be thrown if a required ros parameter has not been provided.
+     */
     class LackingRosParameter : public std::exception {
     private:
-        const std::string parameter_;
+        const std::string parameter_name_;
     public:
-    LackingRosParameter(std::string parameter) : parameter_(parameter) {}
+    LackingRosParameter(std::string parameter_name) : parameter_name_(parameter_name) {}
         std::string get_info()
         {
-            return parameter_;
+            return parameter_name_;
         }
     };
 }
