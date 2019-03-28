310f94103c57b2b0a2442fa76abe61eee513a849
Lukas Kalde <lkalde@gmail.com>
2019-03-28 13:38:46 +0100

    Minor docu for TFBroadcaster

--- src/skeleton3d/src/TFBroadcaster.hpp
+++ src/skeleton3d/src/TFBroadcaster.hpp
@@ -4,11 +4,26 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 
+/**
+ * Broadcasts static TF Frames.
+ *
+ * This class is intended to broadcast frame transforms relative to the global "world" frame.
+ */
 class TFBroadcaster
 {
 public:
     TFBroadcaster() {};
+    /**
+     * Broadcast all currently set frame transformations once.
+     */
     void broadcast();
+
+    /**
+     * Add a frame to the list of frames to broadcast.
+     *
+     * @param frame_transformation Transformation from global to this frame.
+     * @param name Frame name.
+     */
     void add_frame(tf::Transform frame_transformation, std::string name);
     
 private:
